2cce0f010fb594b09540a77ac8ad750ae47da2b8
Rogelio <Rogelio@DESKTOP-F7IKJIP>
2024-04-16 08:02:24 -0300

    ejercicio 11 finalizado

---
+++ tp1_MorañaTapiaHernanJairo/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j,numero=80;
+		
+		for (i=2,j=40;j>0;i++,j--) {
+			System.out.print(numero + " ");
+			numero +=(j-i-1);
+		}
+	}
+
+}
